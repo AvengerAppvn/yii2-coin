560b2ccf4baf25040351136783219103fb01f847
AvengerApp <avengerappvn@gmail.com>
2017-12-25 07:35:07 +0000

    add team

--- backend/views/wallet/_deposit_coin_line.php
+++ backend/views/wallet/_deposit_coin_line.php
@@ -1,5 +1,5 @@
 <div class="row">
-<div class="col-md-4">
+<div class="col-md-3">
     <h1 style="margin-bottom: 30px;padding-left:10%;">
       <img 
         src="<?= $this->assetManager->getAssetUrl($bundle, 'img/tkc.png') ?>" 
@@ -8,7 +8,7 @@
         <?= $code ?>
     </h1>   
 </div>
-<div class="col-md-4">
+<div class="col-md-3">
     <h2 style="line-height: 48px;">
     <button class="btn btn-success btn-lg m-b-sm" style="padding-left:20px;padding-right:20px;"
     type="submit"
@@ -16,8 +16,11 @@
     data-target="#deposit-coin">Deposit</button>
     </h2>
 </div>
-<div class="col-md-4">
-     <h2 class="text-success" style="line-height: 48px;"><?= number_format($wallet->amount_coin,8) ?></h2>
+<div class="col-md-6">
+     <h2 class="text-success" style="line-height: 48px;">
+         <?= number_format($wallet->amount_coin,8) ?>
+         / BONUS <span class="app-btc-bonus"><?= number_format($wallet->amount_bonus,8) ?></span>
+         </h2>
 </div>
 
 </div>
