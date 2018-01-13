43b271ac663327b011c78922f0edb83c5ccc6c38
Lex <lex4vn@gmail.com>
2018-01-13 07:28:39 +0700

    login

--- console/controllers/TeamController.php
+++ console/controllers/TeamController.php
@@ -12,62 +12,63 @@ class TeamController extends Controller
 {
     public function actionIndex()
     {
+        return 0;
         // Scan Deposit
-        $deposits = Deposit::find()->inactive()->all();
-        foreach ($deposits as $deposit) {
-            if (!$deposit->user_id) {
-                $wallet = null;
-                // BTC
-                if ($deposit->type == 1) {
-                    $wallet = Wallet::find()->where(['wallet_btc' => $deposit->receiver]);
-                }
-
-                // ETH
-                if ($deposit->type == 2) {
-                    $wallet = Wallet::find()->where(['wallet_eth' => $deposit->receiver]);
-                }
-
-                if ($wallet) {
-                    $deposit->user_id = $wallet->user_id;
-                }
-
-            }
-
-            if ($deposit->user_id) {
-                // Find parent
-                $teams = Team::find()->where(['related_id' => $deposit->user_id])->all();
-
-                foreach ($teams as $team) {
-
-                    $bonus = $deposit->amount * $this->getRate($team->level);
-
-                    if ($bonus) {
-                        if ($deposit->type == 1) {
-                            $team->amount_btc_bonus += $bonus;
-
-                        } else {
-                            $team->amount_eth_bonus += $bonus;
-                        }
-                        if ($team->save()) {
-                            Yii::error('Plus bonus to Team');
-                            $wallet = $team->wallet;
-                            if ($deposit->type == 1) {
-                                $wallet->bonus_btc = $bonus;
-                            } else {
-                                $wallet->bonus_eth = $bonus;
-                            }
-
-                            $wallet->save();
-                            // TODO save to Event log
-
-                        }
-                    }
-                }
-                $deposit->status = 1;
-                $deposit->save();
-                echo "Switch status, User ID:".$deposit->user_id."\n";
-            }
-        }
+//        $deposits = Deposit::find()->inactive()->all();
+//        foreach ($deposits as $deposit) {
+//            if (!$deposit->user_id) {
+//                $wallet = null;
+//                // BTC
+//                if ($deposit->type == 1) {
+//                    $wallet = Wallet::find()->where(['wallet_btc' => $deposit->receiver]);
+//                }
+//
+//                // ETH
+//                if ($deposit->type == 2) {
+//                    $wallet = Wallet::find()->where(['wallet_eth' => $deposit->receiver]);
+//                }
+//
+//                if ($wallet) {
+//                    $deposit->user_id = $wallet->user_id;
+//                }
+//
+//            }
+//
+//            if ($deposit->user_id) {
+//                // Find parent
+//                $teams = Team::find()->where(['related_id' => $deposit->user_id])->all();
+//
+//                foreach ($teams as $team) {
+//
+//                    $bonus = $deposit->amount * $this->getRate($team->level);
+//
+//                    if ($bonus) {
+//                        if ($deposit->type == 1) {
+//                            $team->amount_btc_bonus += $bonus;
+//
+//                        } else {
+//                            $team->amount_eth_bonus += $bonus;
+//                        }
+//                        if ($team->save()) {
+//                            Yii::error('Plus bonus to Team');
+//                            $wallet = $team->wallet;
+//                            if ($deposit->type == 1) {
+//                                $wallet->bonus_btc = $bonus;
+//                            } else {
+//                                $wallet->bonus_eth = $bonus;
+//                            }
+//
+//                            $wallet->save();
+//                            // TODO save to Event log
+//
+//                        }
+//                    }
+//                }
+//                $deposit->status = 1;
+//                $deposit->save();
+//                echo "Switch status, User ID:".$deposit->user_id."\n";
+//            }
+//        }
     }
 
     private function getRate($level)
