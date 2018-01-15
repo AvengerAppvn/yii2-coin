e8e298603d316984caf2999b8d8e3429d84ea641
Lex <lex4vn@gmail.com>
2018-01-15 17:10:03 +0700

    Referrer

--- backend/views/team/index.php
+++ backend/views/team/index.php
@@ -17,6 +17,10 @@ $this->params['breadcrumbs'][] = $this->title;
 <?php Pjax::begin(); ?>    <?= GridView::widget([
         'dataProvider' => $dataProvider,
         //'filterModel' => $searchModel,
+        'options' => [
+            'class' => 'grid-view table-responsive'
+        ],
+        'showFooter'=>true,
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
             [
@@ -26,17 +30,39 @@ $this->params['breadcrumbs'][] = $this->title;
                 },
                 'filter' => ArrayHelper::map(User::find()->all(), 'id', 'username')
             ],
-            'level',
-            'amount_btc',
-            'amount_btc_bonus',
-            'amount_eth',
-            'amount_eth_bonus',
-            'amount_total_bonus',
+            [
+                'attribute' => 'level',
+                'footer' => 'Total',
+                'value' => function ($model) {
+                    return 'F'.$model->level;
+                }
+            ],
+            //'amount_btc',
+            //'amount_btc_bonus',
+
+            [
+                'attribute' => 'amount_btc_bonus',
+                'footer' => '<b>'.number_format($total_btc_bonus,8).'</b>',
+                'value' => function ($model) {
+                    return $model->amount_btc_bonus;
+                }
+            ],
+            [
+                'attribute' => 'amount_eth_bonus',
+                'footer' => '<b>'.number_format($total_eth_bonus,8).'</b>',
+                'value' => function ($model) {
+                    return $model->amount_eth_bonus;
+                }
+            ],
+            //'amount_eth',
+            //'amount_total_bonus',
             // 'type',
             // 'status',
             // 'created_at',
             // 'updated_at',
-
+//            'footer'=> [
+//                'amount_btc_bonus'=>100
+//            ]
         ],
     ]); ?>
 <?php Pjax::end(); ?></div>
