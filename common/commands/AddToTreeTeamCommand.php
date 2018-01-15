17c9170abb3a2b6bc5ca74e7d53a7b865a861907
Lex <lex4vn@gmail.com>
2018-01-15 14:39:30 +0700

    Referrer

--- common/commands/AddToTreeTeamCommand.php
+++ common/commands/AddToTreeTeamCommand.php
@@ -28,32 +28,35 @@ class AddToTreeTeamCommand extends Object implements SelfHandlingCommand
      */
     public function handle($command)
     {
+        //Yii::error("Check");
         if(Team::find()->where(['user_id'=>$command->user_id,'related_id'=>$command->related_id])->exists()){
             return false;
         }
-        
+        $level = 1;
         $model = new Team();
         $model->user_id = $command->user_id;
         $model->related_id = $command->related_id;
-        $model->level = 1;
+        $model->level = $level;
         $model->save(false);
-        $this->scanRelated($model->user_id,$model->related_id);
+        $this->scanRelated($model->user_id,$model->related_id,$level);
         return 1;
     }
     
-    private function scanRelated($user_id,$primary){
+    private function scanRelated($user_id,$primary,$level){
+        //Yii::error("Scan:".$user_id.':'.$primary);
         // Find parent of related_id
         $user = User::findOne($user_id);
         if($user && $user->referrer){
             // Find level
             $team = Team::find()->where(['user_id'=>$user->referrer,'related_id'=>$user->id])->limit(1)->one();
             if($team){
+                $level++;
                 $model = new Team();
                 $model->user_id = $user->referrer;
                 $model->related_id = $primary;
-                $model->level = $team->level + 1;
+                $model->level = $level;
                 $model->save(false);
-                return $this->scanRelated($user->referrer,$primary);
+                return $this->scanRelated($user->referrer,$primary,$level);
             }
         }
         return false;
